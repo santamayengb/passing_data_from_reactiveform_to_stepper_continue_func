cb467ad2f2faeafeab06a95e18a2b70793a07cd3
santamayengb <19mca8232@cuchd.in>
2022-02-21 13:47:53 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/stepperdatapassing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stepperdatapassing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/textfields_name.dart
@@ -0,0 +1,15 @@
+class TextFieldsName {
+  TextFieldsName._();
+
+  static const restaurantDetails = RestaurantDetails();
+}
+
+class RestaurantDetails {
+  const RestaurantDetails();
+
+  final restaurantName = 'Restaurant Name';
+  final restaurantAddress = 'Restaurant Address';
+  final restaurantAlternateAddress = 'Reastaurant Alternate Address';
+  final phoneNumber = 'Phone Number';
+  final phoneAlternateNumber = 'Alternate Phone Number';
+}
